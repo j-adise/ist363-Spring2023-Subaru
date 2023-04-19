cc6a1f0349ea80c89e435a1472c09debac2e97c1
j-adise <jadise@syr.edu>
2023-04-19 13:52:51 -0400

    first comit

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+
+import Layout from '../components/Layout';
+
+
+import { 
+  //getAllVehicles, 
+  //getAllColors 
+} from '../lib/api';
+
+
+export async function getStaticProps() {
+  // Get external data from the file system, API, DB, etc.
+  //const data = getAllVehicles();
+ // const colors = getAllColors();
+
+  // The value of the `props` key will be
+  //  passed to the `Home` component
+  return {
+    props: {
+      //data,
+      //colors
+    }
+  }
+}
+
+const Homepage = ({ 
+  data, 
+  //colors
+}) => {
+  //console.log({data});
+  return <Layout>
+    {
+    //<Showcase />
+    //<LocationPicker />
+    //<CarColorPicker colors={colors} />
+    //<SectionCTA />
+    }
+  </Layout>
+}
+export default Homepage
